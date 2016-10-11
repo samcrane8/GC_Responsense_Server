6456fd9a98f32407ba71e8398684d02809df09d5
Bruce Crane <samcrane8@gmail.com>
2016-10-11 12:47:33 -0400

    Testing CodeDeploy
    I just want to see if CodeDeploy works. This is all on AWS so you won’t
    see it in the code here, but hopefully we can push changes
    automatically with this.

--- hello_world.js
+++ hello_world.js
@@ -1,6 +1,10 @@
 var http = require('http');
+	fs = require('fs'),
+	base = '/home/examples/public_html';
+
 
 http.createServer(function (request, response) {
+	
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Hello World\n');
 }).listen(8080);
