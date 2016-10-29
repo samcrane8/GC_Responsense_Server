86aefcbaf2d1b5e6c1bb5063af0907b88a6f1533
John Britti <jakebritti@gmail.com>
2016-10-28 23:53:22 -0400

    slowly merging old page to test page

--- test/scripts/scripts.js
+++ test/scripts/scripts.js
@@ -1,18 +1,20 @@
 var loadTarget = true;
+var changed = false;
 $(document).ready(function() {
-	$('.text#0').html(loadText(window.location.hash.substring(1)));
-	$('.text#0').toggleClass('visible');
+	radialButton();
+	$('#main-logo').on('click', function() {
+		console.log(1);
+		$('.side-bar').toggleClass('side');
+	})
+	// $('.text#0').html(loadText(window.location.hash.substring(1))).toggleClass('visible');
 });
 $(window).on('hashchange', function() {
 	loadContent();
 });
 function loadContent() {
-	// console.log('.text' + (loadTarget ? 0 : 1));
 	$('.text#' + (loadTarget ? 0 : 1)).toggleClass('visible');
-
-	// console.log('.text' + (loadTarget ? 1 : 0));
-	$('.text#' + (loadTarget ? 1 : 0)).html(loadText(window.location.hash.substring(1)));
-	$('.text#' + (loadTarget ? 1 : 0)).toggleClass('visible');
+	$('.text#' + (loadTarget ? 1 : 0)).html(loadText(window.location.hash.substring(1))).toggleClass('visible');
+	// $('.text#' + (loadTarget ? 1 : 0)).toggleClass('visible');
 	loadTarget = !loadTarget;
 }
 function loadText(button) {
@@ -27,4 +29,38 @@ function loadText(button) {
 	} else if (button === 'e') {
 		return "e";
 	}
+}
+function radialButton() {
+	$('.button.cent').each(function(index) {
+		$(this).css({
+			'left' : (250 - 120*Math.cos(-0.5 * Math.PI - 2*index/5*Math.PI)).toFixed(4) + "px",
+			'top'  : (250 + 120*Math.sin(-0.5 * Math.PI - 2*index/5*Math.PI)).toFixed(4) + "px"
+		});
+		if (index == 0) {
+			$(this).css({
+				'margin-top' : -0.7*$(this).height(),
+				'margin-left' : -$(this).width() / 2
+			});
+		} else if (index == 1) {
+			$(this).css({
+				'margin-left' : '0px',
+				'text-align'  : 'right'
+			});
+		} else if (index == 2) {
+			$(this).css({
+				'margin-left' : '0px',
+				'text-align'  : 'right'
+			});
+		} else if (index == 3) {
+			$(this).css({
+				'margin-left' : -1*$(this).width(),
+				'text-align'  : 'right'
+			});
+		} else {
+			$(this).css({
+				'margin-left' : -1*$(this).width(),
+				'text-align'  : 'right'
+			});
+		}
+	});
 }
