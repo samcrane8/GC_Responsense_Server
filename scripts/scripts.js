dab598d4a833fc5cb47204cd26f264e5b7a5bc01
Bruce Crane <samcrane8@gmail.com>
2016-12-26 16:30:33 -0500

    Made working buttons
    - set up subclasses to the “text” css class
    - each different subcategory of content is now sitting undisplayed when
    loaded, then is activated when their respective button is clicked.
       - we may want to look into more dynamically loaded means but for now
    this is okay.
    - we also really need to look at the way the opening page is made, the
    arbitrary pixel sizes aren’t scaling well.
    - some basic content for each tab is now written, but that’ll have to
    be more thoroughly written out.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,38 +1,66 @@
 var hasMoved = false;
+
+var buttons = [ 'about', 'map', 'contact', 'team', 'blog' ]
+
 $(document).ready(function() {
 	radial_button();
 	$('.button.side').each(function(index) {
 		$('.button.side').fadeOut(0);
 	});
 	$('#main-logo').on('click', function() {
-		if ($('.logo-nav').hasClass('side')) {
-			$('.text').toggleClass('side');
-			setTimeout(function() {
-				$('.logo-nav').toggleClass('side');
-				$('#nav-buttons').fadeIn( "fast");
-				$('.button.cent').each(function(index) {
-					$(this).delay(170).fadeIn( "slow");
-				});
-				$('.button.side').each(function(index) {
-					$(this).fadeOut( "fast");
-				});
-			}, 500);
-		} else {
-			$('.logo-nav').toggleClass('side');
-			setTimeout(function() { 
-		        $('.text').toggleClass('side');
-		    }, 800);
-			$('#nav-buttons').fadeOut( "fast");
-			$('.button.cent').each(function(index) {
-				$(this).fadeOut( "fast");
-			});
-			$('.button.side').each(function(index) {
-				$(this).delay(170).fadeIn( "slow");
-			});
+		if ($('.logo-nav').hasClass('side')){
+			transition_views();
 		}
-		
+	});
+	$('#about').on('click', function() {
+		$('.text.content.about').css('display','block');
+		transition_views();
+	});
+	$('#map').on('click', function() {
+		$('.text.content.map').css('display','block');
+		transition_views();
+	});
+	$('#contact').on('click', function() {
+		$('.text.content.contact').css('display','block');
+		transition_views();
+	});
+	$('#team').on('click', function() {
+		$('.text.content.team').css('display','block');
+		transition_views();
+	});
+	$('#blog').on('click', function() {
+		$('.text.content.blog').css('display','block');
+		transition_views();
+	});
+
+	$('#nb-map').on('click', function(){
+		makeContentInvisible();
+		$('.text.content.map').css('display','block');
+	});
+	$('#nb-about').on('click', function(){
+		makeContentInvisible();
+		$('.text.content.about').css('display','block');
+	});
+	$('#nb-contact').on('click', function(){
+		makeContentInvisible();
+		$('.text.content.contact').css('display','block');
+	});
+	$('#nb-team').on('click', function(){
+		makeContentInvisible();
+		$('.text.content.team').css('display','block');
+	});
+	$('#nb-blog').on('click', function(){
+		makeContentInvisible();
+		$('.text.content.blog').css('display','block');
 	});
 });
+
+function makeContentInvisible(){
+	for (i = 0; i < buttons.length; i++){
+		$('.text.content.'+buttons[i]).css('display','none');
+	}
+}
+
 $(document).mousemove(function(e){
     $("#hole").css({'left': e.pageX - 300, 'top':e.pageY - 300});
     $("#hm-left").css({'width' : (e.pageX - 200) + 'px'});
@@ -44,6 +72,41 @@ $(document).mousemove(function(e){
     	hasMoved = true;
     }
 });
+
+/*
+*	This function transitions from the beginning radial view to the side view with
+	content on the right and a directional bar on the left. It can also transfer back
+	if already in the side view state.
+*/
+function transition_views(){
+	if ($('.logo-nav').hasClass('side')) {
+			$('.text').toggleClass('side');
+			setTimeout(function() {
+				$('.logo-nav').toggleClass('side');
+				$('#nav-buttons').fadeIn( "fast");
+				$('.button.cent').each(function(index) {
+					$(this).delay(170).fadeIn( "slow");
+				});
+				$('.button.side').each(function(index) {
+					$(this).fadeOut( "fast");
+				});
+			}, 500);
+		makeContentInvisible();
+	} else {
+		$('.logo-nav').toggleClass('side');
+		setTimeout(function() { 
+	        $('.text').toggleClass('side');
+	    }, 800);
+		$('#nav-buttons').fadeOut( "fast");
+		$('.button.cent').each(function(index) {
+			$(this).fadeOut( "fast");
+		});
+		$('.button.side').each(function(index) {
+			$(this).delay(170).fadeIn( "slow");
+		});
+	}
+}
+
 function radial_button() {
 	$('.button.cent').each(function(index) {
 		$(this).css({
